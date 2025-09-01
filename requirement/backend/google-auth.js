7524c64152f5e95bb239e64cfa866e7a011b4a17
besalort <benjamin.salort@hotmail.com>
2025-09-01 18:00:45 +0200

    normalement la connexion google est reparee plus ou moins. Il manque une redirection auto vers le changement de mdp et de finaliser le changement de mdp en question. Un doute sur la methode pour eviter que nimporte qui puisse changer le mdp en utilisant cette route. Faut donc avoir une var unique etc. C'est en travaux

--- requirement/backend/google-auth.js
+++ requirement/backend/google-auth.js
@@ -1,6 +1,9 @@
 //fichier concernant l'authentification OAuth2 avec google
 const axios = require('axios');
 const db = require('./database.js');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
@@ -16,14 +19,22 @@ function generateRandomPassword(length = 16) {
 	return (crypto.randomBytes(length).toString('hex')); // genere un mdp aleatoire de 16 caracteres
 }
 
-
+function generateUniqueUsername(username) {
+	let username_tmp = username;
+	let i = 1;
+	while (db.prepare('SELECT id FROM users WHERE username = ?').get(username_tmp)) {
+		username_tmp = `${username}${i}`;
+		i++;
+	}
+	return (username_tmp);
+}
 
 function registerGoogleAuthRoutes(fastify, hashPassword) {
 
 	// Redirection vers la page Google, declare la route /auth/google, ne renvoie pas de contenu html, redirige directement vers google
 	fastify.get('/auth/google', async (request, reply) => {
 		//
-		const params = new URLSearchParams({ //API javascript construit des chaines de requetes url qutomqtiaue, encode tous les param en format url valide
+		const params = new URLSearchParams({ //API javascript construit des chaines de requetes url automatique, encode tous les param en format url valide
 			client_id: GOOGLE_CLIENT_ID, //identifiant unique de l'app
 			redirect_uri: REDIRECT_URI, // url de redirection apres validation
 			response_type: 'code', // demande d'un code d'autorisation (standard OAuth2)
@@ -51,6 +62,7 @@ function registerGoogleAuthRoutes(fastify, hashPassword) {
 
 		try {
 			//requete http post vers l'endpoint(adresse url vers laquelle on envoie la requete, ici point d'entree de l'api google) de google
+			console.log("Step 1: code recu: ", code);
 			const tokenResponse = await axios.post('https://oauth2.googleapis.com/token', {
 				code, //code envoye
 				client_id: GOOGLE_CLIENT_ID, //identifiant dans .env
@@ -62,6 +74,7 @@ function registerGoogleAuthRoutes(fastify, hashPassword) {
 
 			//on extrait l'access_token des donnees renvoyees par google, on utilise pas le refresh token parce qu'il ne sera pas renvoye a chaque connexion.
 			//le refresh token sert a obtenir un nouvel access token sans repasser par l'etape d'autorisation
+			console.log("Step 2: tokenResponse: ", tokenResponse.data);
 			const { access_token } = tokenResponse.data; // methode de destructuring
 
 			//envoi une requete get a un autre endpoint contenant les infos du user
@@ -70,11 +83,13 @@ function registerGoogleAuthRoutes(fastify, hashPassword) {
 					Authorization: `Bearer ${access_token}` //montre le jeton d'acces a google pour prouver qu'on a le droit d'acceder
 				}
 			});
+			console.log("Step 3: userInfoResponse: ", userInfoResponse.data);
 
 			//destructure la reponse de google, elle contient les infos demandees, le nom de l'user, son adresse, son identifiant unique (Google Subject ID)
 			//sub=subject identifier = identifiant unique donnee par google au user dans l'app/site (unique, stable, lie a l'app/site, non modifiable, ne depend pas du nom ou de l'adresse)
 			//souvent utilise comme cle de ref
-			const { name, email, sub } = userInfoResponse.data; //destructuring
+			const { name : googleName, email, sub } = userInfoResponse.data; //destructuring en mettant le retour de name dans un const googleName
+			console.log("Step 4: info user: ", { googleName, email, sub });
 
 			//interroge la base SQLite, verifie si l'user existe deja, si user = undefined, il faut le creer
 			// let firstTime = false;
@@ -93,7 +108,7 @@ function registerGoogleAuthRoutes(fastify, hashPassword) {
 			// }
 			
 			// Gerer le cas du name deja existant:
-			let username = name;
+			let username = googleName;
 			let firstTime = false;
 			let user = db.prepare('SELECT * FROM users WHERE email = ?').get(email);
 			if (!user)
@@ -101,27 +116,34 @@ function registerGoogleAuthRoutes(fastify, hashPassword) {
 
 			if (firstTime)
 			{
-				name = email; //Le login d'un user google sera toujours son email mais il sera impossible de rentrer a la main un mail donc impossible d'avoir une duplicite
+				let name = email; //Le login d'un user google sera toujours son email mais il sera impossible de rentrer a la main un mail donc impossible d'avoir une duplicite
 				const randomPassword = await hashPassword(generateRandomPassword());
 
 				// Gerer le cas de l'username deja existant:
-				username = name;
-				let username_tmp = username;
-				i = 1;
-				while (db.prepare('SELECT id FROM users WHERE username = ?').get(username_tmp)) {
-					username_tmp = `${username}${i}`;
-					i++;
-				}
-				username = username_tmp;
+				username = generateUniqueUsername(googleName);
+				// let username_tmp = username;
+				// let i = 1;
+				// while (db.prepare('SELECT id FROM users WHERE username = ?').get(username_tmp)) {
+				// 	username_tmp = `${username}${i}`;
+				// 	i++;
+				// }
+				// username = username_tmp;
 				// Creer l'user :
-				user = db.prepare('INSERT INTO users (name, email, username, password_hash) VALUES (?, ?, ?, ?)').run(name, email, username, randomPassword);
+				try {
+					const insert = db.prepare('INSERT INTO users (name, email, username, password_hash) VALUES (?, ?, ?, ?)').run(name, email, username, randomPassword);
+					user = db.prepare('SELECT * FROM users WHERE id = ?').get(insert.lastInsertRowid);				
+				} catch (err) {
+					console.log("Print verif err on insert");
+					console.error(err);
+					throw (err);
+				}
 			}
-			const token = fastify.jwt.sign({ id: user.id, username: user.username });
+			// const token = fastify.jwt.sign({ id: user.id, username: user.username, mustChangePassword: true}, process.env.JWT_KEY!, { expiresIn: "1h" });
 			return {
 				token,
 				name: user.name,
 				username: user.username,
-				firstTime
+				mustChangePassword: firstTime
 			};
 			
 
